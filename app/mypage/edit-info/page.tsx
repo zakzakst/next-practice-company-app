60cb9353667ef173d4b00dd835b1b4e51f4cb3f6
zakzakst <e1433104@gmail.com>
2026-04-25 05:45:36 +0900

    fix

--- app/mypage/edit-info/page.tsx
+++ app/mypage/edit-info/page.tsx
@@ -1,13 +1,48 @@
 "use client";
 
-import { MyInfoForm } from "@/components/features/my/MyInfoForm";
+import { useCallback } from "react";
+
+import { useRouter } from "next/navigation";
+
+import { FormValues, MyInfoForm } from "@/components/features/my/MyInfoForm";
+import { MyData, useGetMyData, usePutMyData } from "@/orval/myData";
+import { toast } from "sonner";
+
+const converter = (data: MyData): FormValues | undefined => {
+  if (!data) return undefined;
+  const { name, phone, email } = data;
+  return {
+    name,
+    phone,
+    email,
+  };
+};
 
 const Page = () => {
+  const router = useRouter();
+  const { data } = useGetMyData();
+  const { trigger } = usePutMyData();
+
+  const handleSubmit = useCallback(
+    async (values: Omit<FormValues, "confirmPassword">) => {
+      await trigger(values);
+      toast("更新しました");
+      router.push("/mypage");
+    },
+    [trigger, router],
+  );
+
   return (
     <div>
       <h1 className="text-2xl font-bold">情報更新</h1>
       <div className="mt-4">
-        <MyInfoForm />
+        {data?.data && (
+          <MyInfoForm
+            values={converter(data.data)}
+            onSubmit={handleSubmit}
+            onReturn={() => router.push("/mypage")}
+          />
+        )}
       </div>
     </div>
   );
--- components/features/my/MyInfoForm/index.tsx
+++ components/features/my/MyInfoForm/index.tsx
@@ -1,23 +1,43 @@
 "use client";
 
-import Link from "next/link";
-
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
 
-const Data = {
-  id: 1,
-  name: "Yamada",
-  department: "○○部",
-  jobTitle: "メンバー",
-  phone: "08000000000",
-  email: "taro@example.com",
-  joinedOn: "2026-04-01",
+export const formSchema = z.object({
+  name: z.string().min(1, { error: "氏名は入力必須項目です" }),
+  phone: z
+    .string()
+    .or(z.literal(""))
+    .refine((val) => val === "" || /^[0-9]{10,11}$/.test(val), {
+      message: "有効な電話番号を入力してください",
+    }),
+  email: z.email({ error: "有効なメールアドレスを入力してください" }),
+});
+
+export type FormValues = z.infer<typeof formSchema>;
+
+type Props = {
+  values?: Partial<FormValues>;
+  onSubmit: (values: FormValues) => void;
+  onReturn: () => void;
 };
 
-export const MyInfoForm = () => {
+export const MyInfoForm = ({ values, onSubmit, onReturn }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid, isDirty, errors },
+  } = useForm<FormValues>({
+    resolver: zodResolver(formSchema),
+    defaultValues: values,
+    mode: "onChange",
+  });
+
   return (
     <Card>
       <CardContent>
@@ -25,28 +45,37 @@ export const MyInfoForm = () => {
           <div className="col-span-2 grid grid-cols-subgrid items-center">
             <Label htmlFor="name">氏名</Label>
             <div>
-              <Input id="name" defaultValue={Data.name} />
+              <Input id="name" {...register("name")} />
+              {errors.name && <p>{errors.name.message}</p>}
             </div>
           </div>
           <div className="col-span-2 grid grid-cols-subgrid items-center">
             <Label htmlFor="phone">電話番号</Label>
             <div>
-              <Input id="phone" defaultValue={Data.phone} />
+              <Input id="phone" maxLength={11} {...register("phone")} />
+              {errors.phone && <p>{errors.phone.message}</p>}
             </div>
           </div>
           <div className="col-span-2 grid grid-cols-subgrid items-center">
             <Label htmlFor="email">メールアドレス</Label>
             <div>
-              <Input id="email" defaultValue={Data.email} />
+              <Input id="email" {...register("email")} />
+              {errors.email && <p>{errors.email.message}</p>}
             </div>
           </div>
         </div>
       </CardContent>
       <CardFooter>
         <div className="flex items-center gap-2">
-          <Button size="lg">更新</Button>
-          <Button size="lg" variant="outline" asChild>
-            <Link href="/mypage">戻る</Link>
+          <Button
+            size="lg"
+            onClick={handleSubmit((values) => onSubmit(values))}
+            disabled={isDirty && !isValid}
+          >
+            送信
+          </Button>
+          <Button size="lg" variant="outline" onClick={onReturn}>
+            戻る
           </Button>
         </div>
       </CardFooter>
